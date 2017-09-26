a65898ef69d221e973c7e49fe43ce395867fac66
vmistyurin <vmistyurin@yandex.ru>
2017-09-27 00:02:32 +0700

    macroses.h added

--- core/include/core/macroses.h
+++ core/include/core/macroses.h
@@ -2,4 +2,13 @@
 #define GRAPH_CORE_MACROSES
 
 #define THROW_IF(condition, exception) if(condition) throw exception;
+
+using ushort = unsigned short;
+
+#ifdef _M_AMD64
+using word = unsigned long long;
+#else
+using word = unsigned int;
+#endif  
+
 #endif
