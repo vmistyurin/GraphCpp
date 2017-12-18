76c8323d4832432e9db0987e7e3b8be071332925
vmistyurin <vmistyurin@yandex.ru>
2017-12-18 22:47:08 +0700

    removed extra macroses

--- core/include/core/macroses.h
+++ core/include/core/macroses.h
@@ -2,14 +2,7 @@
 #define GRAPH_CORE_MACROSES
 
 #define RETURN_IF(condition, value) if(condition) return value;
-using ushort = unsigned short;
-using msize = ushort;
+using msize = size_t;
 using mcontent = unsigned int; //TODO: Template it
-
-#ifdef _M_AMD64
-using word = unsigned long long;
-#else
-using word = unsigned int;
-#endif  
 
 #endif
