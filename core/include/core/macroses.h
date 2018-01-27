bdf25479c012472afe36bfc4db5cc3b71402d5b6
vmistyurin <vmistyurin@yandex.ru>
2018-01-27 22:58:39 +0500

    added cstddef.h to macroses

--- core/include/core/macroses.h
+++ core/include/core/macroses.h
@@ -1,6 +1,8 @@
 #ifndef GRAPH_CORE_MACROSES
 #define GRAPH_CORE_MACROSES
 
+#include <cstddef.h>
+
 #define RETURN_IF(condition, value) if(condition) return value;
 using msize = size_t;
 using mcontent = unsigned int; //TODO: Template it
