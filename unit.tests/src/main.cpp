38645f89e83d51ec15f5f47dfa1f8b26480d51a5
Vyacheslav <ronaldo-13@mail.ru>
2018-03-10 17:38:33 +0700

    print environment in unit tests

--- unit.tests/src/main.cpp
+++ unit.tests/src/main.cpp
@@ -1,15 +1,21 @@
-#include <stdio.h>
+#include <iostream>
 #include "gtest/gtest.h"
 
 int main(int argc, char **argv) 
 {
 #ifdef DEBUG
-	printf("DEBUG");
+	std::cout << "DEBUG" << std::endl;
 #else	
-	printf("RELEASE");
+	std::cout << "RELEASE" << std::endl;
 #endif // DEBUG
 
-	printf("Running main() from gtest_main.cc\n");
+#ifdef _M_X64
+	std::cout << "x64" << std::endl;
+#else 
+	std::cout << "x86" << std::endl;
+#endif // _M_X64
+
+	std::cout << "Running main() from gtest_main.cc" << std::endl;
 	testing::InitGoogleTest(&argc, argv);
 	return RUN_ALL_TESTS();
 }
