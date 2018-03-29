23f9e6ba3ba18e0237975656227f142493acdd50
vmistyurin <vmistyurin@yandex.ru>
2018-03-29 23:22:30 +0700

    test CompareListsWithoutOrder added

--- unit.tests/src/helpers_tests/comparators_tests.cpp
+++ unit.tests/src/helpers_tests/comparators_tests.cpp
@@ -13,7 +13,8 @@ using namespace graphcpp_testing;
 		std::vector<int> permutated_test_vector = { 1,2,4,5,3 };
 		std::vector<int> different_size = { 1,2,3,4 };
 		std::vector<int> different_content = { 1,2,3,4,6 };
-
+		
+		EXPECT_TRUE(compare_vectors_without_order(test_vector, test_vector));
 		EXPECT_TRUE(compare_vectors_without_order(test_vector, permutated_test_vector));
 		EXPECT_FALSE(compare_vectors_without_order(test_vector, different_size));
 		EXPECT_FALSE(compare_vectors_without_order(test_vector, different_content));
@@ -56,6 +57,19 @@ using namespace graphcpp_testing;
 		EXPECT_FALSE(compare_vectors_without_order(test_vector, different_node));
 		EXPECT_FALSE(compare_vectors_without_order(test_vector, more_edges));
 		EXPECT_FALSE(compare_vectors_without_order(test_vector, less_edges));
+	}
+
+	TEST(HelpersTests, CompareListsWithoutOrder)
+	{
+		std::list<int> test_list = { 1,2,3,4,5 };
+		std::list<int> permutated_test_list = { 1,2,4,5,3 };
+		std::list<int> different_size = { 1,2,3,4 };
+		std::list<int> different_content = { 1,2,3,4,6 };
+
+		EXPECT_TRUE(compare_lists_without_order(test_list, test_list));
+		EXPECT_TRUE(compare_lists_without_order(test_list, permutated_test_list));
+		EXPECT_FALSE(compare_lists_without_order(test_list, different_size));
+		EXPECT_FALSE(compare_lists_without_order(test_list, different_content));
 	}
 #endif // USE_HELPERS_TESTS
 
