808dbc20a4b5bdb4248218897d222ab07b10858d
vmistyurin <vmistyurin@yandex.ru>
2018-03-29 15:02:49 +0700

    boost added

--- ci/appveyor/build.cmd
+++ ci/appveyor/build.cmd
@@ -5,4 +5,4 @@ set vs_generator=Visual Studio 15 2017
 
 if not [x%PLATFORM:x64=%]==[x%PLATFORM%] set vs_generator=%vs_generator% Win64
 
-cmake .. -G "%vs_generator%" -DCMAKE_BUILD_TYPE=%CONFIGURATION% -DUSE_ALL_TESTS:BOOL=ON 
+cmake .. -G "%vs_generator%" -DUSE_ALL_TESTS:BOOL=ON -DBOOST_ROOT="C:\Libraries\boost_1_66_0"
