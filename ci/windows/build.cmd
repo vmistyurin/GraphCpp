71f49b03a0ea3616d6e69ae98abd0a585735b50b
vmistyurin <vmistyurin@yandex.ru>
2018-09-06 00:06:20 +0700

    Restore path after build

--- ci/windows/build.cmd
+++ ci/windows/build.cmd
@@ -7,3 +7,5 @@ if not [x%PLATFORM:x64=%]==[x%PLATFORM%] set vs_generator=%vs_generator% Win64
 
 cmake .. -G "%vs_generator%" -DUSE_ALL_TESTS:BOOL=ON -DBOOST_ROOT="C:\Libraries\boost_1_64_0"
 cmake --build .
+
+cd ..
