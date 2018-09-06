672c4f81d4437e30f81811ffc4d52e9f84303147
vmistyurin <vmistyurin@yandex.ru>
2018-09-06 08:43:43 +0700

    Use configuration in build

--- ci/windows/build.cmd
+++ ci/windows/build.cmd
@@ -6,6 +6,6 @@ set vs_generator=Visual Studio 15 2017
 if not [x%PLATFORM:x64=%]==[x%PLATFORM%] set vs_generator=%vs_generator% Win64
 
 cmake .. -G "%vs_generator%" -DUSE_ALL_TESTS:BOOL=ON -DBOOST_ROOT="C:\Libraries\boost_1_64_0"
-cmake --build .
+cmake --build . --config %configuration%
 
 cd ..
